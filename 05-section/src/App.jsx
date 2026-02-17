75d2158f803b4ba7e7752254af37c9c766db31bf
hashimmalik59 <hm3630450@gmail.com>
2026-02-17 23:52:28 +0500

    Added Section 5 components and folder structure

---
+++ 05-section/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// components
+import Pizza from './components/Pizza'
+
+const App = () => {
+    return (
+        <div>
+            <Pizza />
+        </div>
+    )
+}
+
+export default App
---
+++ 05-section/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Tumhara main component
+import './index.css'; // Global styles
+
+// Browser ke 'root' div ko pakro aur React tree render karo
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
