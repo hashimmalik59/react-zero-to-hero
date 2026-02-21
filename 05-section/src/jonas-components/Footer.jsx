00671aba85589ab1c7f538f60fcc588ded715e36
hashimmalik59 <hm3630450@gmail.com>
2026-02-21 14:41:26 +0500

    05-section: Extracting JSX into a component

--- 05-section/src/jonas-components/Footer.jsx
+++ 05-section/src/jonas-components/Footer.jsx
@@ -7,20 +7,31 @@ const Footer = () => {
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
 
-    if (!isOpen) return <small style={{ color: "red" }}>Sorry we're close right now comeback at {openHour}:00</small>
+    if (!isOpen) return <p style={{ color: "red" }}>Sorry we're close right now comeback at {openHour}:00</p>
 
     return (
         <footer>
-            <button>Order Now</button>
-            {/* <small className='bg' style={{
+            {/* <p className='bg' style={{
                 color: isOpen ?
                     "green" : "red"
-            }}>{new Date().toLocaleTimeString()} | {isOpen ? "Welcome we're open" : "Sorry, we're closed!"}</small> */}
+            }}>{new Date().toLocaleTimeString()} | {isOpen ? "Welcome we're open" : "Sorry, we're closed!"}</p> */}
 
             {/* {isOpen ? <strong style={{ color: "green" }}>We're Open until {closeHour}:00, order online from here.</strong> : <strong style={{ color: "red" }}>Sorry we're closed!, Our time is {openHour}:00 to {closeHour}:00</strong>} */}
-
-            <small style={{ color: "green" }}>We're open! Come and visit us or order online</small>
+            <Order
+                openHour={openHour}
+                closeHour={closeHour}
+            />
         </footer>
+    )
+}
+
+const Order = ({ openHour, closeHour }) => {
+    console.log();
+    return (
+        <div className='order'>
+            <button>Order Now</button>
+            <p style={{ color: "green" }}>We're open! from {openHour}:00 to {closeHour}:00.</p>
+        </div>
     )
 }
 
